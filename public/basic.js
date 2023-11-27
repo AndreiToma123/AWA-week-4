3e253dce4056ee0eabf73c00a84596279caba671
AndreiToma123 <Andrei.Toma@student.lut.fi>
2023-11-27 23:03:02 +0200

    .

--- public/basic.js
+++ public/basic.js
@@ -1,19 +1,19 @@
-fetch('http://localhost:8000/recipe/pizza')
+fetch('http://localhost:3000/recipe/pizza')
   .then(response => response.json())
   .then(recipe => {
     document.getElementById('recipe-name').innerText = `${recipe.name}`;
-    const instructionList = document.getElementById('recipe-instructions');
-    const ingredientList = document.getElementById('recipe-ingredients');
+    const instructionsList = document.getElementById('recipe-instructions');
+    const ingredientsList = document.getElementById('recipe-ingredients');
 
     recipe.instructions.forEach(i => {
       const li = document.createElement('li');
       li.innerText = i;
-      instructionList.appendChild(li);
+      instructionsList.appendChild(li);
     });
-
+    
     recipe.ingredients.forEach(i => {
       const li = document.createElement('li');
       li.innerText = i;
-      ingredientList.appendChild(li);
+      ingredientsList.appendChild(li);
     });
-  });
+  })
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 3000;
 
 app.use(express.static('public'));
 
@@ -13,11 +13,12 @@ app.get('/recipe/:food', (req, res) => {
   const foodName = req.params.food;
   const recipeInstructions = ['1) Mix well', '2) Put in the oven', '3) Cool down'];
   const recipeIngredients = ['1) Something fresh', '2) Something healthy'];
+
   const recipe = {
     name: foodName,
     instructions: recipeInstructions,
-    ingredients: recipeIngredients
-    };
+    ingredients: recipeIngredients,
+  };
 
   res.json(recipe);
 });
@@ -27,5 +28,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at port ${port}`);
+  console.log(`Server  listening at port:${port}`);
 });
